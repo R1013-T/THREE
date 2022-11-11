339640869585232aee9866dc4e0ab5d5536aa55c
R1013-T <nosuke1013@icloud.com>
2022-11-12 01:49:14 +0900

    done

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -15,23 +15,23 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-const AuthErrors = (e: string) => {
-  let errorMessage;
-  switch (e) {
-    case "auth/email-already-in-use":
-      errorMessage = "このメールアドレスはすでに登録済みです。";
-      break;
-    case "auth/wrong-password":
-      errorMessage = "メールアドレスまたはパスワードが違います。";
-      break;
-    case "auth/too-many-requests":
-      errorMessage =
-        "ログインに何度も失敗したため、このアカウントへのアクセスは一時的に無効になっています。時間を空けてもう一度試してください。";
-      break;
-    default:
-      errorMessage = e;
-  }
-  return errorMessage;
-};
+// const AuthErrors = (e: string) => {
+//   let errorMessage;
+//   switch (e) {
+//     case "auth/email-already-in-use":
+//       errorMessage = "このメールアドレスはすでに登録済みです。";
+//       break;
+//     case "auth/wrong-password":
+//       errorMessage = "メールアドレスまたはパスワードが違います。";
+//       break;
+//     case "auth/too-many-requests":
+//       errorMessage =
+//         "ログインに何度も失敗したため、このアカウントへのアクセスは一時的に無効になっています。時間を空けてもう一度試してください。";
+//       break;
+//     default:
+//       errorMessage = e;
+//   }
+//   return errorMessage;
+// };
 
-export default AuthErrors;
+// export default AuthErrors;
