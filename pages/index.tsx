23e50d4b7f3285a4b15fe61f039c582b924c7f0e
R1013-T <nosuke1013@icloud.com>
2022-11-12 00:17:45 +0900

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from "next/head";
-import { useState } from "react";
+import styles from "../styles/top.module.scss";
 import AuthHeader from "../components/auth/AuthHeader";
-import styles from "../styles/auth.module.scss";
+
+import { useState } from "react";
+import Head from "next/head";
+
+import { FcGoogle } from "react-icons/fc";
 
 export default function Home() {
-
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -15,9 +17,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <AuthHeader isLoading={isLoading}/>
+      <AuthHeader isLoading={isLoading} />
       <main className={styles.main}>
-        <h1>#Top Page</h1>
+        <div className={styles.inner}>
+          <button className={styles.singup}>Sing up</button>
+          <button className={styles.login}>Log in</button>
+          <div className={styles.or}>or</div>
+          <button className={styles.googleLogin}>
+            <div className={styles.googleInner}>
+              <div>
+                <FcGoogle />
+              </div>
+              <p>Log in with Google</p>
+              <div></div>
+            </div>
+          </button>
+        </div>
       </main>
     </div>
   );
